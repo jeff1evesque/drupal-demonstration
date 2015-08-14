e9589995250e389691855a9e9195fbb0e5a62e9e
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-08-14 08:49:21 -0400

    #140: opcache.pp, add file

---
+++ puppet/manifests/opcache.pp
@@ -0,0 +1,59 @@
+## define $PATH for all execs
+Exec {path => ['/usr/bin/']}
+
+## variables
+$opcache_dependencies = ['php-devel', 'gcc']
+
+## create '/vagrant/build/' directory
+file {'/vagrant/build/':
+    ensure => 'directory',
+    before => Vcsrepo['/vagrant/build/opcache'],
+}
+
+## clone opcache repository
+vcsrepo {'/vagrant/build/opcache':
+    ensure => present,
+    provider => git,
+    source => 'https://github.com/zendtech/ZendOptimizerPlus',
+    revision => 'v7.0.5',
+    before => Package["${opcache_dependencies}"],
+}
+
+## packages: install opcache dependencies (yum)
+package {$opcache_dependencies:
+    ensure => 'installed',
+    provider => 'yum',
+    before => Package[$packages_general_npm],
+}
+
+## prepare opcache: prepare extension for compiling
+exec {'prepare-opcache':
+    command => 'phpsize',
+	refreshonly => true,
+    notify => Exec['configure-opcache'],
+    cwd => '/vagrant/build/opcache/',
+}
+
+## configure opcache: configure the sources
+exec {'configure-opcache':
+    command => './configure --with-php-config=$(which php-config)',
+    refreshonly => true,
+    notify => Exec['compile-opcache'],
+    cwd => '/vagrant/build/opcache/',
+}
+
+## compile opcache: compile the sources
+exec {'compile-opcache':
+    command => 'make',
+    refreshonly => true,
+    notify => Exec['install-opcache'],
+    cwd => '/vagrant/build/opcache/',
+}
+
+## install opcache: install into PHP extension directory
+exec {'install-opcache':
+    command => 'make install',
+    refreshonly => true,
+    #notify => Exec['load-opcache'],
+    cwd => '/vagrant/build/opcache/',
+}
