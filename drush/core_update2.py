b95e59d104ab81316967e44172136cc8b1a77630
Jeffrey Levesque <jeff1evesque@yahoo.com>
2015-09-02 15:56:28 -0400

    #203: core_update2.py, check if file exists before backup, restore

--- drush/core_update2.py
+++ drush/core_update2.py
@@ -3,15 +3,26 @@
 ## @core_update.py
 #  This file performs drupal core update
 import subprocess
+import os.path
+
+## Variables
+htaccess        = '/vagrant/.htaccess'
+htaccess_backup = '/vagrant/_htaccess'
+robots          = '/vagrant/robots.txt'
+robots_backup   = '/vagrant/_robots.txt'
 
 ## Backup '.htaccess', 'robots.txt'
-subprocess.check_call(['mv', '/vagrant/.htaccess', '/vagrant/_htaccess'])
-subprocess.check_call(['mv', '/vagrant/robots.txt', '/vagrant/_robots.txt'])
+if os.path.isfile(htaccess):
+  subprocess.check_call(['mv', htaccess, htaccess_backup])
+if os.path.isfile(robots):
+  subprocess.check_call(['mv', robots, robots_backup])
 
 ## Core Update: update to latest stable core
 subprocess.check_call(['drush', '-y', 'rf'])
 subprocess.check_call(['drush', 'up', '-y', 'drupal'])
 
 ## Restore '.htaccess', 'robots.txt'
-subprocess.check_call(['mv', '/vagrant/_htaccess', '/vagrant/.htaccess'])
-subprocess.check_call(['mv', '/vagrant/_robots.txt', '/vagrant/.robots.txt'])
+if os.path.isfile(htaccess_backup):
+  subprocess.check_call(['mv', htaccess_backup, htaccess])
+if os.path.isfile(robots_backup):
+  subprocess.check_call(['mv', robots_backup, robots])
