e7e4cad78aab08c52cd9d4dab3d0393ead49f3e7
jeff levesque <jeff1evesque@yahoo.com>
2016-01-12 14:22:47 -0500

    #249: configure_php.pp, add phpMyAdmin related logic

--- puppet/environment/development/manifests/configure_php.pp
+++ puppet/environment/development/manifests/configure_php.pp
@@ -2,3 +2,50 @@
 class {'php':
   version => '5.6',
 }
+
+## add EPEL Repository, which allows 'phpmyadmin' to be installed
+exec {'add-epel':
+    command => "yum -y install epel-release --enablerepo=extras",
+    refreshonly => true,
+    notify => Exec['update-yum'],
+    timeout => 450,
+}
+
+## update yum using the added EPEL repository
+exec {'update-yum':
+    command => 'yum -y update',
+    refreshonly => true,
+    notify => Exec['install-phpmyadmin'],
+    timeout => 750,
+}
+
+## install phpmyadmin: requires the above 'add-epel', and 'update-yum'
+exec {'install-phpmyadmin':
+    command => 'yum -y install phpmyadmin',
+    refreshonly => true,
+    notify => Exec['phpmyadmin-access-1'],
+}
+
+## allow phpmyadmin access from guest VM to host (part 1)
+#
+#  Note: this segment appends directly below <Directory /usr/share/phpMyAdmin/>
+exec {'phpmyadmin-access-1':
+    command => 'sed -i "12i\Order allow,deny" /etc/httpd/conf.d/phpMyAdmin.conf',
+    refreshonly => true,
+    notify => Exec['phpmyadmin-access-2'],
+}
+
+## allow phpmyadmin access from guest VM to host (part 2)
+#
+#  Note: this segment appends directly below (part 1)
+exec {'phpmyadmin-access-2':
+    command => 'sed -i "13i\Allow from all" /etc/httpd/conf.d/phpMyAdmin.conf',
+    refreshonly => true,
+    notify => Exec['php-memory-limit'],
+}
+
+## increase php memory limit (i.e. bootstrap 3 theme)
+exec {'php-memory-limit':
+    command => 'sed -i "s/memory_limit = 128M/memory_limit = 512M/" /etc/php.ini',
+    refreshonly => true,
+}
