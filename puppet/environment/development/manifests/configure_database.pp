96607bc882f79514d384cac87b365e49138acc45
jeff levesque <jeff1evesque@yahoo.com>
2016-01-14 12:57:06 -0500

    #286: configure_database.pp, add 'CREATE', 'DROP' for 'authenticated'

--- puppet/environment/development/manifests/configure_database.pp
+++ puppet/environment/development/manifests/configure_database.pp
@@ -31,7 +31,7 @@ class {'::mysql::server':
     'authenticated@localhost/db_drupal.*' => {
       ensure     => 'present',
       options    => ['GRANT'],
-      privileges => ['INSERT', 'DELETE', 'UPDATE', 'SELECT'],
+      privileges => ['INSERT', 'DELETE', 'UPDATE', 'SELECT', 'CREATE', 'DROP'],
       table      => 'db_drupal.*',
       user       => 'authenticated@localhost',
     },
