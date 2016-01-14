ad291464cee2f89a4540b8d29ec8e4733ce2a704
jeff levesque <jeff1evesque@yahoo.com>
2016-01-14 14:07:02 -0500

    #286: configure_php.pp, comment 'Require', add 'Require all granted

--- puppet/environment/development/manifests/configure_php.pp
+++ puppet/environment/development/manifests/configure_php.pp
@@ -78,18 +78,31 @@ package {$php_packages:
 #
 #  Note: this segment appends directly below <Directory /usr/share/phpMyAdmin/>
 exec {'phpmyadmin-access-1':
-    command => 'sed -i "/^<Directory \/usr\/share\/phpMyAdmin\/>$/a \   Order allow,deny" /etc/httpd/conf.d/phpMyAdmin.conf',
+    command => 'sed -i "/^Require ip ::1$/a \       Require all granted" /etc/httpd/conf.d/phpMyAdmin.conf',
     refreshonly => true,
-    notify => Exec['phpmyadmin-access-2'],
+    notify => Exec['phpmyadmin-comment-require-1'],
 }
 
-## allow phpmyadmin access from guest VM to host (part 2)
-#
-#  Note: this segment appends directly below (part 1)
-exec {'phpmyadmin-access-2':
-    command => 'sed -i "/^<Directory \/usr\/share\/phpMyAdmin\/>$/a \   Allow from all" /etc/httpd/conf.d/phpMyAdmin.conf',
+## allow phpmyadmin access: comment out unnecessary 'require' statements
+exec {'phpmyadmin-comment-require-1':
+    command => 'awk "/<RequireAny>/,/<\/RequireAny>/ { if (/Require ip 127.0.0.1/) \$0 = \"#Require ip 127.0.0.1\" }1"  /etc/httpd/conf.d/phpMyAdmin.conf > /home/vagrant/phpMyAdmin.tmp',
     refreshonly => true,
-    notify => Exec['php-memory-limit'],
+    notify => Exec['phpmyadmin-comment-require-2'],
+}
+exec {'phpmyadmin-comment-require-2':
+    command => 'mv /home/vagrant/phpMyAdmin.tmp /etc/httpd/conf.d/phpMyAdmin.conf',
+    refreshonly => true,
+    before => Exec['phpmyadmin-comment-require-3'],
+}
+exec {'phpmyadmin-comment-require-3':
+    command => 'awk "/<RequireAny>/,/<\/RequireAny>/ { if (/Require ip ::1/) \$0 = \"#Require ip ::1\" }1"  /etc/httpd/conf.d/phpMyAdmin.conf > /home/vagrant/phpMyAdmin.tmp',
+    refreshonly => true,
+    notify => Exec['phpmyadmin-comment-require-4'],
+}
+exec {'phpmyadmin-comment-require-4':
+    command => 'mv /home/vagrant/phpMyAdmin.tmp /etc/httpd/conf.d/phpMyAdmin.conf',
+    refreshonly => true,
+    before => Exec['php-memory-limit'],
 }
 
 ## increase php memory limit (i.e. bootstrap 3 theme)
