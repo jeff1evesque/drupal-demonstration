9f06a07c5aba69f9a426c3cf5d5ce22f6912074e
jeff levesque <jeff1evesque@yahoo.com>
2016-01-18 10:35:14 -0500

    #212: core_update2.py, dynamically add path for drush commands

--- drush/core_update2.py
+++ drush/core_update2.py
@@ -20,8 +20,8 @@ if os.path.isfile(robots):
   subprocess.check_call(['mv', robots, robots_backup])
 
 ## Core Update: update to latest stable core
-subprocess.check_call(['drush', '-y', 'rf', '-r', '/vagrant/webroot'])
-subprocess.check_call(['drush', 'up', 'drupal', '-y', '-r', '/vagrant/webroot'])
+subprocess.check_call(['drush', '-y', 'rf', '-r', webroot])
+subprocess.check_call(['drush', 'up', 'drupal', '-y', '-r', webroot])
 
 ## Restore '.htaccess', 'robots.txt'
 if os.path.isfile(htaccess_backup):
