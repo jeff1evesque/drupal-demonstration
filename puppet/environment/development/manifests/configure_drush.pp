d355091f4d777261334a14dea28b21272f0e9ee3
jeff levesque <jeff1evesque@yahoo.com>
2016-02-21 16:49:18 -0500

    #363: configure_drush.pp, remove 'include wget'

--- puppet/environment/development/manifests/configure_drush.pp
+++ puppet/environment/development/manifests/configure_drush.pp
@@ -1,9 +1,6 @@
 ## define $PATH for all execs
 Exec {path => ['/usr/bin/', '/usr/local/']}
 
-## include puppet modules
-include wget
-
 ## install, and enable drush
 class drush {
     ## install composer: drush installation dependency
