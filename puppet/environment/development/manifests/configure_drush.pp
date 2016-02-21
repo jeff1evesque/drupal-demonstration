6c1c7ad6288b51652c3c50ee19874e45e347c855
jeff levesque <jeff1evesque@yahoo.com>
2016-02-21 17:43:15 -0500

    #363: configure_drush.pp, define alias bc no drush attribute works

--- puppet/environment/development/manifests/configure_drush.pp
+++ puppet/environment/development/manifests/configure_drush.pp
@@ -1,3 +1,9 @@
+## variables
+$user = 'provisioner'
+
+## define $PATH for all execs
+Exec {path => ['/usr/sbin/']}
+
 ## install, and enable drush
 class drush {
     ## drush dependency
@@ -6,7 +12,21 @@ class drush {
     ## install drush
     drush::drush { 'drush':
         version    => '8',
-        target_dir => '/usr/bin'
+        target_dir => '/usr/bin',
+    }
+
+    ## define drush alias
+    exec {'drush-alias':
+        command => "echo 'alias drush="sudo /usr/local/bin/drush"' >> /home/${user}/.bashrc",
+        refreshonly => true,
+        notify => Exec['reload-bash-startup-config'],
+    }
+
+    ## reload bash startup files
+    exec {'reload-bash-startup-config':
+        command => 'source ~/.bashrc',
+        refreshonly => true,
+        provider => 'shell',
     }
 }
 
