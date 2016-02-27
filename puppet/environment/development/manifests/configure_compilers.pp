022e0f88a602af12a48fd096ff365d8d80f15963
jeff levesque <jeff1evesque@yahoo.com>
2016-02-27 12:43:53 -0500

    #368: configure_compilers.pp, install nodejs

--- puppet/environment/development/manifests/configure_compilers.pp
+++ puppet/environment/development/manifests/configure_compilers.pp
@@ -37,8 +37,18 @@ $build_environment = 'development'
 $path_source       = '/vagrant/src'
 $path_asset        = '/vagrant/webroot/sites/all/themes/custom/sample_theme/asset'
 
+## include puppet modules
+class install_nodejs {
+    class { 'nodejs':
+        repo_url_suffix => 'node_5.x',
+    }
+}
+
 ## install necessary packages
 class install_packages {
+    ## set dependency
+    require install_nodejs
+
     ## variables
     $packages_general  = ['inotify-tools', 'ruby-devel']
     $packages_npm      = ['uglify-js', 'node-sass', 'imagemin']
@@ -59,6 +69,7 @@ class install_packages {
 ## create necessary directories
 class create_directories {
     ## set dependency
+    require install_nodejs
     require install_packages
 
     ## create log directory
@@ -82,6 +93,7 @@ class create_directories {
 ## create compilers
 class create_compilers {
     ## set dependency
+    require install_nodejs
     require install_packages
     require create_directories
 
@@ -164,6 +176,7 @@ class create_compilers {
 
 ## constructor
 class constructor {
+    contain install_nodejs
     contain install_packages
     contain create_directories
     contain create_compilers
