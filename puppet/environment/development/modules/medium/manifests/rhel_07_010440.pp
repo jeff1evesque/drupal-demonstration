68c6d3932951c09a91ade73e52f64936b42759ad
jeff levesque <jeff1evesque@yahoo.com>
2016-03-08 20:09:04 -0500

    #434: rhel_07_010440.pp, add file

---
+++ puppet/environment/development/modules/medium/manifests/rhel_07_010440.pp
@@ -0,0 +1,64 @@
+### ID: rhel_07_010440
+### Severity: Medium
+###
+### Description: Failure to restrict system access to authenticated users
+###              negatively impacts operating system security.
+###
+### Fix: Configure the operating system to not allow an unattended or automatic
+###      remote logon to the system.
+###
+###      Edit the appropriate “sshd_config” file to uncomment or add the lines
+###      for the “PermitEmptyPasswords”, “PermitUserEnvironment”,
+###      “HostbasedAuthentication” keywords and set their values to “no”:
+###
+###      PermitEmptyPasswords no
+###      PermitUserEnvironment no
+###      HostbasedAuthentication no
+###
+### Note: the prefix 'high::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class high::rhel_07_010440 {
+    ## variables
+    $sshd_file = '/etc/ssh/sshd_config'
+
+    ## ensure line
+    file_line { 'prevent-empty-password':
+		path  => $sshd_file,
+        line => 'PermitEmptyPasswords no',
+   	    match => '^.*PermitEmptyPasswords.*',
+    }
+
+    ## ensure line
+    file_line { 'prevent-user-environment':
+		path  => $sshd_file,
+        line => 'PermitUserEnvironment no',
+   	    match => '^.*PermitUserEnvironment.*',
+    }
+
+    ## ensure line
+    file_line { 'prevent-host-authentication':
+		path  => $sshd_file,
+        line => 'HostbasedAuthentication no',
+   	    match => '^.*HostbasedAuthentication.*',
+    }
+
+    ## remove duplicate(s)
+    exec { 'remove-empty-password':
+        command => "sed  -i -e '/PermitEmptyPassword.*no/b' -i -e '/PermitEmptyPassword/d' ${sshd_file}",
+        path    => '/usr/bin',
+    }
+
+    ## remove duplicate(s)
+    exec { 'remove-user-environment':
+        command => "sed  -i -e '/PermitUserEnvironment.*no/b' -i -e '/PermitUserEnvironment/d' ${sshd_file}",
+        path    => '/usr/bin',
+    }
+
+    ## remove duplicate(s)
+    exec { 'remove-host-authentication':
+        command => "sed  -i -e '/HostbasedAuthentication.*no/b' -i -e '/HostbasedAuthentication/d' ${sshd_file}",
+        path    => '/usr/bin',
+    }
+}
