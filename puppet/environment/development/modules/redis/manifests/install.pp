0f9b08cb64bda11f06fbba75ab39bb16dcdcefa6
jeff levesque <jeff1evesque@yahoo.com>
2016-04-25 10:36:28 -0400

    #317: install.pp, install redis from 'thomasvandoren'

--- puppet/environment/development/modules/redis/manifests/install.pp
+++ puppet/environment/development/modules/redis/manifests/install.pp
@@ -1,7 +1,10 @@
-### Note: the prefix 'vagrant::', corresponds to a puppet convention:
+### Note: the prefix 'redis::', corresponds to a puppet convention:
 ###
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class redis::install {
-    class { 'redis': }
+    include redis
+    class { 'redis':
+        version => '2.6.5',
+    }
 }
