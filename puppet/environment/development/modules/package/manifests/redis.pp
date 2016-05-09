27a58b75e693b9fbe135c73e6fa6dbd231e1d36f
jeff levesque <jeff1evesque@yahoo.com>
2016-05-09 15:35:08 -0400

    #317: redis.pp, install redis '2.6.16'

--- puppet/environment/development/modules/package/manifests/redis.pp
+++ puppet/environment/development/modules/package/manifests/redis.pp
@@ -3,8 +3,7 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class package::redis {
-    include redis
-    class { 'redis':
-        version => '2.6.5',
+    class { 'redis:
+        package_ensure => '2.6.16',
     }
 }
