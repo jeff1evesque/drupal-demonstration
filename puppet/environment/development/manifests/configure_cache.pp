9ffc82b360242792ff630adba0a84535702209b7
jeff levesque <jeff1evesque@yahoo.com>
2016-05-09 09:51:00 -0400

    #317: configure_cache.pp, add file

---
+++ puppet/environment/development/manifests/configure_cache.pp
@@ -0,0 +1,9 @@
+### configure_cache.pp: install, and configure caching for drupal.
+###
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+
+## redis
+include redis::install
