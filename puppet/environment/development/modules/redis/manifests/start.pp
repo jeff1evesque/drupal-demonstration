270d5ebc9b667ed641d826e8df3884817b32cc1f
jeff levesque <jeff1evesque@yahoo.com>
2016-05-13 16:22:52 -0400

    #317: start.pp, add file

---
+++ puppet/environment/development/modules/redis/manifests/start.pp
@@ -0,0 +1,10 @@
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class redis::start {
+    exec { 'start-redis':
+        command => 'redis-sever',
+        path    => '/usr/bin',
+    }
+}
