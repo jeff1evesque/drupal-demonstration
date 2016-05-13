c72b6b9a3fbce9436a5c016eda9c22cf122a9334
jeff levesque <jeff1evesque@yahoo.com>
2016-05-13 17:24:22 -0400

    #317: phpredis.pp, add file

---
+++ puppet/environment/development/modules/package/manifests/phpredis.pp
@@ -0,0 +1,48 @@
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class package::phpredis {
+    ## include necessary modules
+    require git
+
+    ## local variables
+    $cwd = '/vagrant'
+
+    ## download phpredis
+    vcsrepo { $cwd:
+        ensure   => present,
+        provider => git,
+        source   => 'https://github.com/phpredis/phpredis',
+        revision => '2.2.7',
+    }
+
+    ## install phpredis
+    exec { 'install-phpredis-phpize':
+        command => 'phpize',
+        cwd     => $cwd,
+        path    => '/usr/bin',
+        notify  => Exec['install-phpredis-configure'],
+    }
+    exec { 'install-phpredis-configure':
+        command     => './configure',
+        cwd         => $cwd,
+        refreshonly => true,
+        notify      => Exec['install-phpredis-make'],
+    }
+    exec { 'install-phpredis-make':
+        command     => 'make && make install',
+        cwd         => $cwd,
+        path        => '/usr/bin',
+        refreshonly => true,
+    }
+
+    ## remove downloaded phpredis
+    file { 'remove-remnant-phpredis':
+        ensure  => absent,
+        path    => "${cwd}/phpredis",
+        recurse => true,
+        purge   => true,
+        force   => true,
+    }
+}
