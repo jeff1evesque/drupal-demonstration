7fbb93ec6779dc1845c3e95b1adb8dfdc144b6b7
jeff levesque <jeff1evesque@yahoo.com>
2016-05-23 14:17:37 -0400

    #464: install.pp, add file

---
+++ puppet/environment/development/modules/drupal/manifests/install.pp
@@ -0,0 +1,29 @@
+### Note: the prefix 'high::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class drupal::install {
+    ## local variables
+    $db_user     = 'root'
+    $db_pass     = 'password'
+    $address     = 'localhost'
+    $port        = '6686'
+    $db          = 'db_drupal'
+    $drupal_user = 'admin'
+    $drupal_pass = 'password'
+    $site_name   = 'sample'
+    $site_email  = 'sample.email@domain.com'
+    $locale      = 'us'
+    $timezone    = 'America/New York'
+
+    ## combined logical variable
+    $sql = "--db-url=mysql://${db_user}/${db_pass}@${address}:${port}/${db}"
+    $acc = "--account-name=${drupal_user} --account-pass=${drupal_pass}"
+    $tmz = "date_default_timezone=${timezone}"
+
+    ## install drupal
+    exec { 'install-drupal':
+        command => "drush site-install ${sql} ${acc} ${tmz}",
+        path    => '/usr/local/bin/drush',
+    }
+}
