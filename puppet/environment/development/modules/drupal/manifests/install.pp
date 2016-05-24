81b5156155b62a69115fb88d74af5ebacd27a138
jeff levesque <jeff1evesque@yahoo.com>
2016-05-24 09:56:56 -0400

    #471: install.pp, implement 'install_profile'

--- puppet/environment/development/modules/drupal/manifests/install.pp
+++ puppet/environment/development/modules/drupal/manifests/install.pp
@@ -4,17 +4,18 @@
 ###
 class drupal::install {
     ## local variables
-    $db_user     = 'root'
-    $db_pass     = 'password'
-    $address     = 'localhost'
-    $port        = '6686'
-    $db          = 'db_drupal'
-    $drupal_user = 'admin'
-    $drupal_pass = 'password'
-    $site_name   = 'sample'
-    $site_email  = 'sample.email@domain.com'
-    $locale_val  = 'us'
-    $webroot     = '/vagrant/webroot'
+    $db_user         = 'root'
+    $db_pass         = 'password'
+    $address         = 'localhost'
+    $port            = '6686'
+    $db              = 'db_drupal'
+    $drupal_user     = 'admin'
+    $drupal_pass     = 'password'
+    $site_name       = 'sample'
+    $site_email      = 'sample.email@domain.com'
+    $locale_val      = 'us'
+    $install_profile = 'sample'
+    $webroot         = '/vagrant/webroot'
 
     ## combined variables
     $sql    = "--db-url=mysql://${db_user}:${db_pass}@${address}:${port}/${db}"
@@ -22,9 +23,12 @@ class drupal::install {
     $info   = "--site-name=${site_name} --site-mail=${site_email}"
     $locale = "--locale=${locale_val}"
 
+    ## combined arguments
+    $arguments = "${install_profile} ${sql} ${acc} ${info} ${locale}"
+
     ## install drupal
     exec { 'install-drupal':
-        command => "drush site-install ${sql} ${acc} ${info} ${locale} -y",
+        command => "drush site-install $arguments -y",
         path    => ['/usr/local/bin', '/usr/bin/'],
         cwd     => $webroot,
     }
