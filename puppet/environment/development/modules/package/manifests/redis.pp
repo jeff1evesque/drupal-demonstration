e09fa1394c590818485fc7f43667e47e710b06a5
jeff levesque <jeff1evesque@yahoo.com>
2016-05-26 19:59:52 -0400

    #317&: redis.pp, adjust redis server build

--- puppet/environment/development/modules/package/manifests/redis.pp
+++ puppet/environment/development/modules/package/manifests/redis.pp
@@ -3,7 +3,35 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class package::redis {
-    class { 'redis::install':
-        redis_version => '3.2.0',
+    ## local variables
+    $root        = '/vagrant'
+    $cwd         = "${root}/build"
+    $verion      = '3.2.0'
+    $server_path = "http://download.redis.io/releases/redis-${version}.tar.gz"
+
+    ## download redis
+    #
+    #  @verbose, defines the verbose attribute for the 'wget' command:
+    #
+    #      https://github.com/maestrodev/puppet-wget/blob/master/manifests/fetch.pp#L166
+    #
+    wget::fetch { 'download-redis-server':
+        source      => "${server_path}",
+        destination => $cwd,
+        timeout     => 0,
+        verbose     => false,
+    }
+
+    ## build redis server
+    exec { 'make-redis-server':
+        command => 'make',
+        cwd     => $cwd,
+        path    => '/usr/bin',
+        notify  => Exec['make-install-redis-server'],
+    }
+    exec { 'make-install-redis-server':
+        command     => 'make install',
+        cwd         => $cwd,
+        refreshonly => true,
     }
 }
