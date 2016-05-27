64484738978e7f0ed151f87ff35f06432daefc28
jeff levesque <jeff1evesque@yahoo.com>
2016-05-27 06:28:59 -0400

    #317: local.settings.php, adjust 'redis_client_host'

--- src/default/local.settings.php
+++ src/default/local.settings.php
@@ -4,7 +4,7 @@
  * Redis configuration
  */
 $conf['redis_client_interface'] = 'PhpRedis'; // Can be "Predis".
-$conf['redis_client_host']      = '1.2.3.4';  // Your Redis instance hostname.
+$conf['redis_client_host']      = 'localhost';  // Your Redis instance hostname.
 $conf['lock_inc']               = 'sites/all/modules/redis/redis.lock.inc';
 $conf['path_inc']               = 'sites/all/modules/redis/redis.path.inc';
 $conf['cache_backends'][]       = 'sites/all/modules/redis/redis.autoload.inc';
