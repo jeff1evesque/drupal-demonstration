2dd9c593984113d8e46dcd85d3377a24052e8332
jeff levesque <jeff1evesque@yahoo.com>
2016-05-28 12:59:26 -0400

    #479: configure.pp, ensure systemd script created

--- puppet/environment/development/modules/redis/manifests/configure.pp
+++ puppet/environment/development/modules/redis/manifests/configure.pp
@@ -2,7 +2,19 @@
 ###
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
-class redis::allow_socket {
+class redis::configure {
+    ## local variables
+    $template_path     = 'redis/server.erb'
+    $build_environment = 'development'
+    $script            = 'redis_server'
+
+    ## dos2unix systemd: convert clrf (windows to linux) in case host
+    #                    machine is windows.
+    file { "/etc/init/redis-systemd.conf":
+        ensure  => file,
+        content => dos2unix(template($template_path)),
+    }
+
     ## selinux: allow httpd to make socket connections
     exec { 'selinux-allow-httpd-socket':
         command => '/usr/sbin/setsebool httpd_can_network_connect=1',
