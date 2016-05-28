19375fa98c28e10658c47c506d4b9474d81fbcf1
jeff levesque <jeff1evesque@yahoo.com>
2016-05-28 08:16:59 -0400

    #477: start.pp, add comment

--- puppet/environment/development/modules/redis/manifests/start.pp
+++ puppet/environment/development/modules/redis/manifests/start.pp
@@ -7,6 +7,7 @@ class redis::start {
     $root     = 'root'
     $log_file = "${root}/vagrant/log/redis_server.log"
 
+    ## background process writes stderr to stdout, and stdout to log
     exec { 'start-redis':
         command => 'redis-server >> ${log_file} 2>&1 &',
         path    => '/usr/local/bin',
