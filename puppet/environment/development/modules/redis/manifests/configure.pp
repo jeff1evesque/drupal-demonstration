cb792487dd3607782e6b07eafc5de1cc6333eea7
jeff levesque <jeff1evesque@yahoo.com>
2016-05-28 15:48:35 -0400

    #479: configure.pp, use single quotes

--- puppet/environment/development/modules/redis/manifests/configure.pp
+++ puppet/environment/development/modules/redis/manifests/configure.pp
@@ -10,7 +10,7 @@ class redis::configure {
 
     ## dos2unix systemd: convert clrf (windows to linux) in case host
     #                    machine is windows.
-    file { "/etc/init/redis-systemd.conf":
+    file { '/etc/init/redis-systemd.conf':
         ensure  => file,
         content => dos2unix(template($template_path)),
     }
