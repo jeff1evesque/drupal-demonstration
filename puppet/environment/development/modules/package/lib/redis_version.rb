b77130f1275779fdb255b9add88cdefb4d2e899f
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-19 13:37:14 -0400

    #512: redis_version.rb, interpolate 'redis_version'

--- puppet/environment/development/modules/package/lib/redis_version.rb
+++ puppet/environment/development/modules/package/lib/redis_version.rb
@@ -1,6 +1,9 @@
+# local variables
+redis_version = '2.8'
+
 Facter.add(:redis_version) do
     confine :osfamily => 'RedHat'
     setcode do
-        Facter::Core::Execution.execute("/usr/bin/yum list redis\* | /usr/bin/grep 2.8 | /usr/bin/awk '{print $2}'")
+        Facter::Core::Execution.execute("/usr/bin/yum list redis\* | /usr/bin/grep #{$redis_version} | /usr/bin/awk '{print $2}'")
     end
 end
