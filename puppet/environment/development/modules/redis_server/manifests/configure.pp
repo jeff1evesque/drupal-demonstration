d124b61ae4fd100d8f049ab63120b96797be2002
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-19 22:36:07 -0400

    #512: configure.pp, rename module name to 'redis_server'

---
+++ puppet/environment/development/modules/redis_server/manifests/configure.pp
@@ -0,0 +1,34 @@
+### Note: the prefix 'package::', corresponds to a puppet convention:
+###
+###       https://github.com/jeff1evesque/machine-learning/issues/2349
+###
+class redis_server::configure {
+    ## local variables
+    $root_dir          = '/vagrant'
+    $provisioner       = 'provisioner'
+    $template_path     = 'redis_server'
+    $build_environment = 'development'
+    $script            = 'redis_server'
+    $environment_dir   = "${root_dir}/puppet/environment/${environment}"
+
+    ## dos2unix systemd: convert clrf (windows to linux) in case host
+    #                    machine is windows.
+    file { '/etc/systemd/system/redis.service':
+        ensure  => file,
+        content => dos2unix(template("${template_path}/server.erb")),
+        mode    => '770',
+    }
+    file { '/etc/systemd/system/redis-initialize.service':
+        ensure  => file,
+        content => dos2unix(template("${template_path}/initialize.erb")),
+        mode    => '770',
+    }
+
+    ## dos2unix bash script: convert clrf (windows to linux) in case host
+    #                    machine is windows.
+    file { "${environment_dir}/scripts/${script}":
+        ensure  => file,
+        content => dos2unix(file("${environment_dir}/scripts/${script}")),
+        mode    => '770',
+    }
+}
