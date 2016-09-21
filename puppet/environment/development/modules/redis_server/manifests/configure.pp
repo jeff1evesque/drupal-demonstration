87c0a403a9d110eefa9dfa758096120008b4a32c
Jeff Levesque <jeff1evesque@yahoo.com>
2016-09-21 18:47:11 -0400

    #511: configure.pp, reduce syntax

--- puppet/environment/development/modules/redis_server/manifests/configure.pp
+++ puppet/environment/development/modules/redis_server/manifests/configure.pp
@@ -3,29 +3,20 @@
 ###       https://github.com/jeff1evesque/machine-learning/issues/2349
 ###
 class redis_server::configure {
-    ## local variables
-    $root_dir          = '/vagrant'
-    $provisioner       = 'provisioner'
-    $template_path     = 'redis_server'
-    $build_environment = 'development'
-    $script            = 'redis_server'
-    $environment_dir   = "${root_dir}/puppet/environment/${environment}"
-
-    ## dos2unix bash script: convert clrf (windows to linux) in case host
-    #                    machine is windows.
-    file { "${environment_dir}/scripts/${script}":
-        ensure  => file,
-        content => dos2unix(file("${environment_dir}/scripts/${script}")),
-        mode    => '770',
-    }
-
     ## selinux for redis
     exec { 'selinux-redis':
         command => '/usr/sbin/setsebool -P httpd_can_network_connect=1',
+        notify  => Exec['redis-daemonize'],
     }
 
     ## ensure redis start on successive boot
-    service { 'redis-server':
-        enable => true,
+    #
+    #  Note: the puppet 'service' directive won't work, since the 'redis'
+    #        module has already defined this service, without persistence.
+    #
+    exec { 'redis-daemonize':
+        command     => 'systemctl enable redis.service',
+        refreshonly => true,
+        path        => '/usr/bin',
     }
 }
