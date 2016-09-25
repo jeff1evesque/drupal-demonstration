f70e814a0e973b7548d1c673826c96d3ce0164cc
System Administrator <jeff1evesque@yahoo.com>
2016-09-25 15:22:54 -0400

    #530: redis_version.rb, replace doube with single quotes

--- puppet/environment/development/modules/package/lib/facter/redis_version.rb
+++ puppet/environment/development/modules/package/lib/facter/redis_version.rb
@@ -5,6 +5,6 @@ redis_version = '2.8'
 Facter.add(:redis_version) do
   confine :osfamily => 'RedHat'
   setcode do
-    Facter::Core::Execution.exec("yum list -q redis\*-#{redis_version}\* | tail -n+2")
+    Facter::Core::Execution.exec('yum list -q redis\*-#{redis_version}\* | tail -n+2')
   end
 end
