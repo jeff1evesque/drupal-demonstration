455b19ab0820b2c4596bb87eaa2f64878166cf8e
System Administrator <jeff1evesque@yahoo.com>
2016-09-25 13:00:28 -0400

    #530: redis_version.rb, reduce syntax with '| tail'

--- puppet/environment/development/modules/package/lib/facter/redis_version.rb
+++ puppet/environment/development/modules/package/lib/facter/redis_version.rb
@@ -4,6 +4,6 @@ redis_version = '2.8'
 Facter.add(:redis_version) do
   confine :osfamily => 'RedHat'
   setcode do
-    Facter::Core::Execution.exec("yum list redis\*-#{redis_version}\* | grep 'redis.x86_64' | awk '{print $2}'")
+    Facter::Core::Execution.exec("yum list -q redis\*-#{redis_version}\* | tail -n+2")
   end
 end
